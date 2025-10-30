3c2d1caa8902bb28985a0cdcece54bffc64d434c
Carlos de Paula <me@carlosedp.com>
2025-10-30 10:59:17 -0300

    Add support for an exclude API for Lua plugins allowing the used of exclude patterns externally

--- yazi-actor/src/mgr/mod.rs
+++ yazi-actor/src/mgr/mod.rs
@@ -12,6 +12,7 @@ yazi_macro::mod_flat!(
 	download
 	enter
 	escape
+	exclude_add
 	filter
 	filter_do
 	find
--- yazi-dds/src/spark/spark.rs
+++ yazi-dds/src/spark/spark.rs
@@ -43,6 +43,7 @@ pub enum Spark<'a> {
 	EscapeSearch(yazi_parser::VoidOpt),
 	EscapeSelect(yazi_parser::VoidOpt),
 	EscapeVisual(yazi_parser::VoidOpt),
+	ExcludeAdd(yazi_parser::mgr::ExcludeAddOpt),
 	Filter(yazi_parser::mgr::FilterOpt),
 	FilterDo(yazi_parser::mgr::FilterOpt),
 	Find(yazi_parser::mgr::FindOpt),
@@ -227,6 +228,7 @@ impl<'a> IntoLua for Spark<'a> {
 			Self::EscapeSearch(b) => b.into_lua(lua),
 			Self::EscapeSelect(b) => b.into_lua(lua),
 			Self::EscapeVisual(b) => b.into_lua(lua),
+			Self::ExcludeAdd(b) => b.into_lua(lua),
 			Self::Filter(b) => b.into_lua(lua),
 			Self::FilterDo(b) => b.into_lua(lua),
 			Self::Find(b) => b.into_lua(lua),
@@ -394,6 +396,7 @@ try_from_spark!(yazi_parser::mgr::CreateOpt, mgr:create);
 try_from_spark!(yazi_parser::mgr::DisplaceDoOpt, mgr:displace_do);
 try_from_spark!(yazi_parser::mgr::DownloadOpt, mgr:download);
 try_from_spark!(yazi_parser::mgr::EscapeOpt, mgr:escape);
+try_from_spark!(yazi_parser::mgr::ExcludeAddOpt, mgr:exclude_add);
 try_from_spark!(yazi_parser::mgr::FilterOpt, mgr:filter, mgr:filter_do);
 try_from_spark!(yazi_parser::mgr::FindArrowOpt, mgr:find_arrow);
 try_from_spark!(yazi_parser::mgr::FindDoOpt, mgr:find_do);
